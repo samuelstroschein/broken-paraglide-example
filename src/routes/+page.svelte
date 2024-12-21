d94b259e58e3da56008dbd053d268cd8e1f2d5c0
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-12-21 18:40:46 -0500

    fix: argument

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
-    import * as m from '$lib/paraglide/messages.js';
+  import * as m from "$lib/paraglide/messages.js";
 </script>
 
-<span>{m.hello_world("Bobini")}</span>
+<span>{m.hello_world({ name: "Bobini" })}</span>
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
