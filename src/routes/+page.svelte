07b2752146d89bc608dac4cb9c86baffe22b665c
Tomasz Kalisiak <tomasz.kalisiak3@gmail.com>
2024-12-22 00:26:33 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import * as m from '$lib/paraglide/messages.js';
+</script>
+
+<span>{m.hello_world("Bobini")}</span>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
