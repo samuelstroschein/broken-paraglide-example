7eea7244ff7b613cdb70a3f4f863fc3c4235085f
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2024-12-21 19:01:54 -0500

    minimal reproduction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,8 @@
 <script>
   import * as m from "$lib/paraglide/messages.js";
+  import { page } from "$app/stores";
 </script>
 
 <span>{m.hello_world({ name: "Bobini" })}</span>
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+
+<p>current url path is {$page.url}</p>
